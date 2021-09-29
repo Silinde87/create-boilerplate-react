c562c1c7f34ed7ed8ed411405d382ae37a3ff682
Pau Rodriguez <pau.rodriguez@singularcover.com>
2021-09-29 21:32:20 +0200

    Split pipeline's build yml into two yml and add lint pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 // jest.config.js
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/**/*.test.js'],
-    coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '/src/index.js',
-    ],
-    coverageReporters: ['json', 'html'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/**/*.test.js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/index.js'],
+  coverageReporters: ['json', 'html'],
 };
