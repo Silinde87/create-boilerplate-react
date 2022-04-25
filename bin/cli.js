e1b9217b83bbb5559b709d190a1b34024583d886
Silinde87 <paurodriguezmolina@gmail.com>
2022-04-25 15:00:35 +0200

    feat: Add project properties to package.json and remove it on cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -17,16 +17,25 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/Silinde87/create-boilerplate-react ${repoName}`;
 const installDepsCommand = `cd ${repoName} && yarn install`;
 const removeBinCommand = `cd ${repoName} && rm -rf bin`
+const gitCommand = `git add . && git commit -m "Repository ${repoName} created`
 
 // Cloning repository
-console.log(`cloning the repository with name ${repoName}`);
+console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if(!checkedOut) process.exit(-1);
 
-// Changing app name at package.json
+// Setting default package.json
+const keysToDelete = ['description', 'author', 'license', 'repository', 'bin', 'keywords'];
 const packageJSON = JSON.parse(fs.readFileSync(`./${repoName}/package.json`, 'utf8'));
 packageJSON.name = repoName;
+packageJSON.version = "0.1.0";
+packageJSON.private = true;
+keysToDelete.forEach(key => delete packageJSON[key]);
 fs.writeFileSync(`./${repoName}/package.json`, JSON.stringify(packageJSON, null, 2));
+
+// Committing changes
+const changesCommited = runCommand(gitCommand);
+if (!changesCommited) process.exit(-1);
 
 // Installing dependencies
 console.log(`Installing dependencies for ${repoName}`);
