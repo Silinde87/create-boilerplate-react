15677b3399faaa2060094d97ed8716b686896ba1
Pau Rodriguez <pau.rodriguez@singularcover.com>
2022-04-25 11:39:46 +0200

    fix: use yarn command instead of npm

--- bin/cli.js
+++ bin/cli.js
@@ -14,7 +14,7 @@ const runCommand = command => {
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/Silinde87/create-boilerplate-react ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const installDepsCommand = `cd ${repoName} && yarn install`;
 
 console.log(`cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
@@ -25,4 +25,4 @@ const installedDeps = runCommand(installDepsCommand);
 if(!installedDeps) process.exit(-1);
 
 console.log(`Congratulations! You are ready. Follow the following commands to start`);
-console.log(`cd ${repoName} && npm start`);
+console.log(`cd ${repoName} && yarn start`);
